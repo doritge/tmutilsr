597ecac264e9636bf9d963221f502df2439cf1dd
doritge <dorit.geifman@gmail.com>
2019-03-04 10:55:08 +0200

    Re-write lda_arrange_docs

--- R/table_utils.R
+++ R/table_utils.R
@@ -1,38 +1,50 @@
-#' Expose document info by their lead topic
+#' Arrange the topic distribution per document
 #'
-#' For each document creates a table of documents associated with the highest
-#' ranking topic and its gamma value. Optionally, The function extends the info
-#' with the document metadata and lower ranking topics. Topic numbers can be
-#' replaced with topic names
+#' Create a table were each row lists a document and its topic distribution
+#' (gamma) in a descending order
 #'
 #' @param lda An LDA object
-#' @param n_docs Number of returned documents per topic. If 0 returns all
-#'   documents
-#' @param ranks Ranks of topics to return
-#' @param topic_names A 2 column tibble - topic (key) and topic name
-#' @param metadata A data frame of document metadata. Key column - document
+#' @param n_ranks Number of topic ranks (<=k)
+#' @param n_docs Number of returned documents per highest ranking topic. By
+#'   default reurns all documents
 #'
-#' @return A table of n_docs documents per topic, each associated with the
-#'   requested info
+#' @return A table of n_docs documents per main topic, each associated with
+#'   n_ranks topic and its gamma
 #'
-#' @importFrom dplyr group_by ungroup arrange filter row_number
+#' @import dplyr
+#' @import tidytext
+#' @import topicmodels
+#'
 #' @importFrom magrittr %>%
+#' @importFrom rlang %@%
 #'
 #' @export
-lda_expose_docs <- function(lda, n_docs = 0, ranks = 1, topic_names = NULL, metadata = NULL){
-    table <- lda %>%
-        tidytext::tidy(matrix = "gamma") %>%
-        group_by(document) %>%
-        arrange(-gamma) %>%
-        filter(dplyr::row_number() == ranks) %>%
-        ungroup()
-    if (!is.null(metadata)) table <- left_join(table, metadata)
-    if (n_docs == 0) return(table)
+lda_arrange_docs <- function(lda, n_ranks = 1, n_docs = 0){
+    # Look at the topics from the document perspective
 
-    table %>%
-        group_by(topic) %>%
-        arrange(-gamma) %>%
-        filter(row_number() <= n_docs) %>%
-        ungroup() %>%
-        arrange(topic, -gamma)
+    k <- LDA_test %@% k
+    docs_table <- tibble(document = LDA_test %@% documents)
+
+    if(n_ranks > k) n_ranks <- k
+
+    lda_table <- LDA_test %>%
+        tidy(matrix = "gamma") %>%
+        arrange(document, -gamma) %>%
+        group_by(document)
+
+    for (i in 1:n_ranks) {
+        tmp <- filter(lda_table, row_number() == i) %>%
+            rename_at(vars(topic, gamma), function(x) c(paste0("rank", i), paste0("gamma", i)))
+        docs_table <- left_join(docs_table, tmp)
+    }
+
+    docs_table <- docs_table %>%
+        arrange(rank1, -gamma1)
+
+    if (n_docs != 0){
+        docs_table <- docs_table %>%
+            group_by(rank1) %>%
+            filter(row_number() <= n_docs)
+    }
+    return(docs_table)
 }
--- tmp_script.R
+++ tmp_script.R
@@ -1,15 +1,113 @@
 
-library(readr)
+# library(readr)
 library(tmutilsr)
+library(tidytext)
+library(magrittr)
+# library(tidyr)
+library(topicmodels)
 
 
 # a test LDA object with 10 topics, 1814 documents & 8008 terms
-LDA_test <- read_rds("LDA_test.rds")
+LDA_test <- read_rds("LDA_filtered_all_10.rds")
+
+tidy(LDA_test, matrix = "gamma")
 
 
-doc_table <- lda_expose_docs(LDA_test)
+doc_table <- lda_arrange_docs(LDA_test, n_ranks = 2, n_docs = 2)
 
 usethis::use_data(LDA_test)
 
 data("LDA_test")
 
+lda_arrange_docs <- function(lda, n_docs = 0, n_ranks = 1){
+    print(n_ranks)
+    print(n_docs)
+    table <- lda %>%
+        tidytext::tidy(matrix = "gamma") %>%
+        group_by(document) %>%
+        arrange(-gamma) %>%
+#        filter(dplyr::row_number() <= n_ranks) %>%
+        ungroup()
+    aa <- split(table, topic)
+
+#    if (n_docs == 0) return(table)
+}
+
+    table <- table %>%
+        group_by(topic) %>%
+        arrange(-gamma) %>%
+        filter(dplyr::row_number() <= n_docs) %>%
+        ungroup() %>%
+        arrange(topic, -gamma)
+    return(table)
+}
+n_ranks<-2
+table <- LDA_test %>%
+    tidytext::tidy(matrix = "gamma") %>%
+    group_by(document) %>%
+    arrange(-gamma) %>%
+    filter(dplyr::row_number() <= n_ranks) %>%
+    ungroup()
+a1<- table %>%
+    group_by(document) %>%
+    filter(dplyr::row_number() == 1) %>%
+    rename(T1 = topic, gamma1 = gamma) %>%
+    ungroup()
+
+a2<- table %>%
+    group_by(document) %>%
+    filter(dplyr::row_number() == 2) %>%
+    rename(T2 = topic, gamma2 = gamma) %>%
+    ungroup()
+
+aa<-dplyr::left_join(a1, a2)
+
+aa <- tibble(document = LDA_test %@% documents) # (package rlang)
+for (i in 1:n_ranks){
+    tabgroup_by(document) %>%
+        filter(dplyr::row_number() == 1) %>%
+        rename(T1 = topic, gamma1 = gamma) %>%
+        ungroup()
+}
+####
+
+library(dplyr)
+library(rlang)
+library(readr)
+library(tidytext)
+library(topicmodels)
+
+lda_arrange_docs <- function(lda, n_ranks = 1, n_docs = 0){
+    # Look at the topics from the document perspective
+
+    k <- LDA_test %@% k
+    docs_table <- tibble(document = LDA_test %@% documents)
+
+    if(n_ranks > k) n_ranks <- k
+
+    lda_table <- LDA_test %>%
+        tidy(matrix = "gamma") %>%
+        arrange(document, -gamma) %>%
+        group_by(document)
+
+    for (i in 1:n_ranks) {
+        tmp <- filter(lda_table, row_number() == i) %>%
+            rename_at(vars(topic, gamma), function(x) c(paste0("rank", i), paste0("gamma", i)))
+        docs_table <- left_join(docs_table, tmp)
+    }
+
+    docs_table <- docs_table %>%
+        arrange(rank1, -gamma1)
+
+    if (n_docs != 0){
+        docs_table <- docs_table %>%
+            group_by(rank1) %>%
+            filter(row_number() <= n_docs)
+    }
+    return(docs_table)
+}
+
+LDA_test <- read_rds("../VR rehabilitation/LDA_filtered_all_10.rds")
+aa <- lda_arrange_docs(LDA_test, n_ranks = 2, n_docs = 2)
+
+####
